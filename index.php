350f1b4dcd4fef3c5a2771d7681c37f0320e6461
Luisedlebond <luisedlebond@gmail.com>
2018-10-29 20:40:57 +0100

    Update index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Graficas</title>
+</head>
+<body>
+	<canvas id="myCanvas" style="border: 1px solid #d3d3d3;">Your Browser does not support the canvas element.</canvas>
+</body>
+</html>
