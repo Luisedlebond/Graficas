c8d77e003b2357a3e366d210f0da6260217e0557
Luisedlebond <luisedlebond@gmail.com>
2018-10-29 20:45:10 +0100

    Ejes
    anado ejes de la grafica

--- index.php
+++ index.php
@@ -9,6 +9,17 @@
 		var canvas = document.getElementById("myCanvas");
 		canvas.width=1000;
 		canvas.height=1000;
+		//anado ejes transversales
+		drawLine(0,-500,0,500);
+		drawLine(-500,0,500,0);
+		function drawLine(startX,startY,endX,endY) {
+			//dibujamos una linea desde la punto de partida
+			canvas.getContext("2d").beginPath();
+			//movemos el punto (0,0) en el centro de la grafica
+			canvas.getContext("2d").moveTo(startX+500,500-startY);
+			canvas.getContext("2d").lineTo(endX+500,500-endY);
+			canvas.getContext("2d").stroke();
+		}
 	</script>
 </body>
 </html>
