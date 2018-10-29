bb59c78ca0a2f9599f7e38d72ad338b188304036
Luisedlebond <luisedlebond@gmail.com>
2018-10-29 20:41:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,10 @@
 </head>
 <body>
 	<canvas id="myCanvas" style="border: 1px solid #d3d3d3;">Your Browser does not support the canvas element.</canvas>
+	<script type="text/javascript">
+		var canvas = document.getElementById("myCanvas");
+		canvas.width=1000;
+		canvas.height=1000;
+	</script>
 </body>
 </html>
